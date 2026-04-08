a9e45169c0e49c668037a4f67c5de0631f3342bc
Jason12jjjj <limjc-wm24@student.tarc.edu.my>
2026-04-08 14:29:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -497,13 +497,13 @@ if app_mode == "📸 Scan & Solve":
                 detected, debug_img, method = extract_colors_from_image(active_buffer, CENTER_COLORS[current_face])
                 
                if detected is None:
-                    st.error(f"❌ **Auto-Detect Failed:** {debug_img}")
-                    
+                    st.error(f"❌ **Auto-Detect Failed:** {debug_img}") 
                     st.info("💡 **Quick Fix:** Click the `>` icon top-left to open the Sidebar, and turn **OFF** `Auto-Detect Cube`. This gives you a manual grid that ignores all background mess!")
                     
                     if current_face in st.session_state.processed_photos:
                         del st.session_state.processed_photos[current_face]
                     st.stop()
+                    s
                 if detected[4] != CENTER_COLORS[current_face]:
                     st.error(f"❌ **Wrong Face Scanned!** Your camera detected a **{detected[4]}** center, but this is the **{current_face} ({CENTER_COLORS[current_face]})** slot.")
                     if current_face in st.session_state.processed_photos:
